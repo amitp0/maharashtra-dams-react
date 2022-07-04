4984a7910d60f4757c529a537e5e329c552da18c
Amit Parkar <83871869+amitp0@users.noreply.github.com>
2022-07-05 00:17:44 +0530

    Created Gauge.js

---
+++ src/components/Gauge.js
@@ -0,0 +1,13 @@
+import { useRef } from "react";
+import {select} from 'd3-selection';
+
+function Gauge(){
+    const svgRef=useRef();
+
+    return(
+    <>
+    </>
+    );
+}
+
+export default Gauge;
